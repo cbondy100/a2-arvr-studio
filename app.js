a42ab32375a3d50d88b55eddb314bd88019da63e
Callum Bondy <callum.bondy@gmail.com>
2023-04-18 18:13:27 -0500

    new push
    Co-authored-by: Mark Joseph Leonardi <MarkLeonardi2023@u.northwestern.edu>
    Co-authored-by: Mark Joseph Leonardi <mjl1296@ads.northwestern.edu>

--- app.js
+++ app.js
@@ -24,39 +24,174 @@ window.onload = function(e) {
 
       // database
       const db = firebase.database()
+      const game = db.ref("game")
+
+      const newPlayerRef = game.child('players').push();
+
+      const gameState = game.child('gameState');
+
+      const wordRef = newPlayerRef.child('word');
+
+      let player = {
+        name: "", 
+        word: "",
+        submitted: false,
+        joined: false
+      }
       
       // creating our new Vue app
       new Vue({
         template: `<div id = "app"> 
-          <div>STARTER PAGE</div> 
-          <button @click="joinGame" v-id="!joined">join</button>
+        
+          <p> Player</p>
+          <input v-model="newPlayerRef.name" />
+          <button @click="joinGame" :disabled="!canEnterName">Join Game</button>
+          <button @click="startGame" :disabled="canStartGame">Start Game</button>
+          <button @click="resetGame">RESET GAME</button>
+          <div hidden={}
+          <div v-show="gameState">
+            <p> LETS PLAY A GAME <p> 
+            <input v-model="newPlayerRef.word" />
+            <button @click="submitWord"> Submit Word </button>
+            <div id="guesses"> 
+            </div>
           </div>`,
         computed: {
-            joined() {
-              return this.userKey
+
+          
+
+          canStartGame() {
+            game.child('players').on('value', (snapshot) => {
+              const players = snapshot.val()
+              const num_players = snapshot.numChildren()
+              if (num_players == 2){
+                return true
+              } else {
+                return false
+              }
+            })
+          },
+
+          canEnterName() {
+            if(this.newPlayerRef.name != null && this.newPlayerRef.name != ""){
+              return true
+            } else {
+              return false
             }
+          }
+
         },
 
         methods: {
             // game methods
             // join game method
-            joinGame() {
-              const userRef = this.pla
+            joinGame(){
+
+              if(newPlayerRef.name != "") {
+                console.log("Player joined game")
+              
+                newPlayerRef.set({
+                  id: newPlayerRef.key,
+                  name: newPlayerRef.name,
+                  submitted: false,
+                })
+                wordRef.set("")
+              } else {
+                console.log("Can not join game without player name")
+              }
+
+              // make buttons not clickable now
+            },
+
+            startGame(){
+              console.log("starting game")
+              gameState.set(true)
+            },
+
+            resetGame() {
+              // From ChatGPT
+              db.ref('game').remove().then(function() {
+                console.log("Database cleared successfully.");
+                gameState.set(false)
+                // from chatGPT
+                while (guesses.firstChild) {
+                  guesses.removeChild(guesses.firstChild);
+              }
+
+              }).catch(function(error) {
+                console.error("Error clearing database:", error);
+              });
+            },
+
+            submitWord() {
+              this.newPlayerRef.update({
+                word: newPlayerRef.word
+              })
             }
         },
 
+        watch: {
+          "newPlayerRef.name" () {
+            console.log("New Name:", this.newPlayerRef.name)
+        },
+ ///         "player2.name" () {
+  ///          console.log("New Name:", this.player2.name)
+//          }
+        },
         
 
         data() {
           return {
-            userKey: undefined,
-            players: {},
-            gameState: 'notStarted'
+
+            newPlayerRef: newPlayerRef,
+            gameState: gameState,
+   ///         player1: player1,
+    ///        player2: player2
           }
         },
 
         mounted() {
-          let myGameRef = db.ref()
+
+          const guesses = document.getElementById("guesses");
+
+        },
+
+        created() {
+
+            game.child('gameState').on('value', (snapshot) => {
+              this.gameState = snapshot.val()
+              console.log("state", this.gameState)
+            })
+            
+            game.child('players').on('value', (snapshot) => {
+              var count = 0
+              var word_list = []
+              var word = null
+              snapshot.forEach(function(childSnapshot) {
+                word = childSnapshot.child('word').val();
+                if(word){
+                  word_list.push(word)
+                  console.log("WORD", word_list)
+                  count++;
+                  console.log("COUNT", count)
+                }
+
+                if(count == 2) {
+                  guess = document.createElement("div");
+                  guess.innerText = "You guys guessed " + word_list[0] + " and " + word_list[1] + "!";
+                  guesses.appendChild(guess);
+                  count = 0
+                  newPlayerRef.update({
+                    word: ""
+                  })
+                  word_list = []
+                  word = null
+                }
+                
+              });
+
+            })
+          
         },
         el: "#app",
       })
